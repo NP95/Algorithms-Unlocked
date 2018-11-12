a442e099377b5dc136f1ae9bc60792d4eba9f1fe
SpawnTree <sumit-google@google.com>
2018-11-13 01:01:34 +0530

    Code Template MAC OSX

---
+++ Code Template/Headers/exception_handler.h
@@ -0,0 +1,13 @@
+//
+//  exception_handler.h
+//  Google_Code
+//
+//  Created by SUMIT  LAHIRI on 11/4/18.
+//  Copyright © 2018 Google PPLNS. All rights reserved.
+//
+
+#ifndef exception_handler_h
+#define exception_handler_h
+#include "static.h"
+
+#endif /* exception_handler_h */
---
+++ Code Template/Headers/functions.h
@@ -0,0 +1,13 @@
+//
+//  functions.h
+//  Google_Code
+//
+//  Created by SUMIT  LAHIRI on 11/4/18.
+//  Copyright © 2018 Google PPLNS. All rights reserved.
+//
+
+#ifndef functions_h
+#define functions_h
+#include "static.h"
+#include "methods.h"
+#endif /* functions_h */
