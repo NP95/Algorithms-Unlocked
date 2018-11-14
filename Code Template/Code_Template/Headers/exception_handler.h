5a2049bf2a804b2b3da2bd63e29c3d647512acd7
Sumit Lahiri <SpawnTree@users.noreply.github.com>
2018-11-14 10:23:19 +0530

    Update exception_handler.h

--- Code Template/Code_Template/Headers/exception_handler.h
+++ Code Template/Code_Template/Headers/exception_handler.h
@@ -10,4 +10,29 @@
 #define exception_handler_h
 #include "static.h"
 
+class RuntimeException
+{
+private:
+	string errorMsg;
+public:
+	RuntimeException(const string& err)
+	{
+		errorMsg = err;
+	}
+	string getMessage() const
+	{
+		return errorMsg;
+	}
+};
+
+class FATAL_EXCEPTION : public RuntimeException {
+public:
+	std::stringstream ss;
+	FATAL_EXECPTION(const string& err) : RuntimeException(err)
+	{
+		ss << err;
+		std::cerr << ss.str();
+	}
+};
+
 #endif /* exception_handler_h */
