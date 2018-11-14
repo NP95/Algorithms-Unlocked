5fc47967004e2af38c3ab496b968f214e4e92668
Sumit Lahiri <SpawnTree@users.noreply.github.com>
2018-11-14 09:24:42 +0530

    Update code.cpp

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -4,20 +4,12 @@ int main(int argc, char* argv[])
 {
     start_time = clock();
     ioutil();
-<<<<<<< HEAD
     /* Your Code here */
     
-=======
-    random_logger();
->>>>>>> 50f6c09ec5ca23875d3985c927b21537844e5307
-    
+   
     
     end_time = clock() - start_time;
-<<<<<<< HEAD
     log(end_time, argc, argv);
-=======
-    std::cerr << "Running. " << argc << " " << argv[1] << " Time : " << ((float)end_time)/CLOCKS_PER_SEC << " sec, " << "Ticks : " << end_time << std::endl;
->>>>>>> 50f6c09ec5ca23875d3985c927b21537844e5307
     close();
     return 0;
 }
