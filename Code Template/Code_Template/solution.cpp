2600c471d6e36575f612b6b98d15d3580564317e
SpawnTree <sumit-google@google.com>
2018-11-17 00:45:13 +0530

    Code X-Code Mac Push

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -128,12 +128,10 @@ auto rd_ints = std::bind(random_engine_block, seed);
 /* Appended working code here using freopen() */
 
 
-#define DFLAGS "flgs"
-
 void solution(int argc, char* argv[], char* envp[])
 {
     /* Write Code here */
-    std::cout << DFLAGS << std::endl;
+    std::cout << "--no-output" << std::endl;
 }
 
 
