2cad982374fb9dd2fcd67a655002a6e37f9ee080
SpawnTree <sumit-google@google.com>
2018-11-18 00:35:02 +0530

    X-Code Mac 7656ab154753b43aa049e1d56c87e833
    dda86fdcfb843912f6255e59633ae96eec941505af8b129135ae3db98375f270

--- Code Template/Code_Template/Headers/io.h
+++ Code Template/Code_Template/Headers/io.h
@@ -15,7 +15,7 @@ static void write_random_ints(const string &file_name = STDIN_TEST_REDIRECT, boo
 {
     std::fstream file(file_name.c_str(), std::fstream::out);
     std::cerr << "Writing ints." << std::endl;
-    for(auto i = 0; i < 10000; i++){
+    for(auto i = 0; i < 1000; i++){
         for(auto j = 0; j < 10000; j++){
             if(!small)
                 file << rd_ints() << " ";
