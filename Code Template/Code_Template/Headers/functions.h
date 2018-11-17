fce757cea12d1b9fbca0aad90d06d512a552ad84
SpawnTree <sumit-google@google.com>
2018-11-17 10:00:17 +0530

    Code X-Code Mac Push

--- Code Template/Code_Template/Headers/functions.h
+++ Code Template/Code_Template/Headers/functions.h
@@ -16,19 +16,34 @@ static string to_hex(unsigned char s) {
 }
 
 #ifdef HASH
-static string sha256(string line) {
+static string sha256(string &line) {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, line.c_str(), line.length());
     SHA256_Final(hash, &sha256);
     
-    string output = "";
+    string output = "0x";
     for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         output += to_hex(hash[i]);
     }
     return output;
 }
+
+static string double_sha256(string &line) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, line.c_str(), line.length());
+    SHA256_Final(hash, &sha256);
+    SHA256_Final(hash, &sha256);
+    string output = "0x";
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        output += to_hex(hash[i]);
+    }
+    return output;
+}
+
 #endif
 
 static void fork_and_run(char * const args[]) {
