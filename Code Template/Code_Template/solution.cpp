960204403bb886055e6e71f7d27ae4afdee4f7ef
SpawnTree <sumit-google@google.com>
2018-11-18 21:09:53 +0530

    X-Code Mac a3293f1b9d716a97667cb5816d65d535
    af0d7feaa237a0383207693d914359e099f6b7674a51d0e2b1e4bca0257a1579

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -177,3 +177,26 @@ auto rd_ints = std::bind(random_engine_block, seed);
 
 /* Appended working code here using freopen() */
 
+
+void solution(int argc, char* argv[], char* envp[])
+{
+ 
+}
+
+
+int main(int argc, char* argv[], char* envp[])
+{
+#ifdef OFFLINE_START_BUILD
+    start_time = clock();
+    ioutil("testcase");
+    random_logger();
+#endif
+    solution(argc, argv, envp);
+    // compare_files_sha256(STDIN_TEST_REDIRECT, STDOUT_CODECHEF_REDIRECT);
+#ifdef OFFLINE_START_BUILD
+    copy_code();
+    log_time(start_time, argc, argv, envp);
+#endif
+    return 0;
+}
+
