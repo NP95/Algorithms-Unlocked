1f3bfb476c02a31102d0e13240bcae64d8bd32ab
SpawnTree <sumit-google@google.com>
2018-11-20 22:01:24 +0530

    X-Code Mac 2ea82f954e0ae6146f00c118efa7234a
    177beda39f241a44919561f0fbfdc492ef73f15b85cb2b775b4b2d01bea87af4

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -203,13 +203,13 @@ int main(int argc, char* argv[], char* envp[])
     start_time = clock();
     intialize_io("testcase");
     build_log();
-    HackerRank();
-    // codejam();
+    // HackerRank(); /* Unzip and copy files for input */
+    // codejam(); /* Unzip and copy files for input */
 #endif
     solution(argc, argv, envp);
 #ifdef OFFLINE_START_BUILD
     build_solution();
-    std::cerr << HackerRank_Compare() << std::endl;
+    // std::cerr << HackerRank_Compare() << std::endl; /* Automated comapre of output files */
     end_time_log(start_time, argc, argv, envp);
 #endif
     return 0;
