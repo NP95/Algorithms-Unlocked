8eeecf4b378add975628e4cce4a604f1244f9f24
SpawnTree <sumit-google@google.com>
2018-11-20 22:05:45 +0530

    X-Code Mac 032c7a6a4f23b91616d1baeb3e017d9e
    919d14b244928d1eb2ab4b9c76ec0cbc62cc94353039d0c74221498410c2ff61

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -191,27 +191,3 @@ auto rd_ints = std::bind(random_engine_block, seed);
 
 /* Appended working code here using freopen() */
 
-
-void solution(int argc, char* argv[], char* envp[])
-{
-
-}
-
-int main(int argc, char* argv[], char* envp[])
-{
-#ifdef OFFLINE_START_BUILD
-    start_time = clock();
-    intialize_io("testcase");
-    build_log();
-    // HackerRank(); /* Unzip and copy files for input */
-    // codejam(); /* Unzip and copy files for input */
-#endif
-    solution(argc, argv, envp);
-#ifdef OFFLINE_START_BUILD
-    build_solution();
-    // std::cerr << HackerRank_Compare() << std::endl; /* Automated comapre of output files */
-    end_time_log(start_time, argc, argv, envp);
-#endif
-    return 0;
-}
-
