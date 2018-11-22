324a1d5883bf372141e392422804f5f716e9b96d
SpawnTree <sumit-google@google.com>
2018-11-22 12:06:37 +0530

    X-Code Mac 891b43afd762a4f1f5420e550ff3fe08
    752e59244d7c8125d2af4bb8270b7c09acbd4ae3dd7e7a9e4790caf7011224b9

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -1,26 +1,8 @@
 #include "Headers/headers.h"
-#define LARGEMOD 998244353
-LL bicoloring[1004][2008][4];
 
 void solution(int argc, char* argv[], char* envp[])
 {
-    int n = 0, k = 0; std::cin >> n >> k;
-    bicoloring[1][1][0] = 1; // WW has 1 component.
-    bicoloring[1][2][1] = 1; // WB has 2 components.
-    bicoloring[1][2][2] = 1; // BW has 2 components.
-    bicoloring[1][1][3] = 1; // BB has 1 component.
-    /* What happens on adding successive columns */
-    for(auto i = 2; i <= n; ++i ) // columns
-    {
-        for(auto j = 1; j <= 2 * i; ++j ) // components
-        {
-            bicoloring[i][j][0] = (bicoloring[i - 1][j    ][0] + bicoloring[i - 1][j    ][1] + bicoloring[i - 1][j    ][2] + bicoloring[i - 1][j - 1][3]) % LARGEMOD;
-            bicoloring[i][j][1] = (bicoloring[i - 1][j - 1][0] + bicoloring[i - 1][j    ][1] + bicoloring[i - 1][j - 2][2] + bicoloring[i - 1][j - 1][3]) % LARGEMOD;
-            bicoloring[i][j][2] = (bicoloring[i - 1][j - 1][0] + bicoloring[i - 1][j - 2][1] + bicoloring[i - 1][j    ][2] + bicoloring[i - 1][j - 1][3]) % LARGEMOD;
-            bicoloring[i][j][3] = (bicoloring[i - 1][j - 1][0] + bicoloring[i - 1][j    ][1] + bicoloring[i - 1][j    ][2] + bicoloring[i - 1][j    ][3]) % LARGEMOD;
-        }
-    }
-    std::cout << (bicoloring[n][k][0] + bicoloring[n][k][1] + bicoloring[n][k][2] + bicoloring[n][k][3]) % LARGEMOD << std::endl;
+
 }
 
 int main(int argc, char* argv[], char* envp[])
