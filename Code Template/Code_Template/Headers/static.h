53b86c615d00493b25eee6c0fe84d2deffa35ba3
System Administrator <root@SUMITs-Mac-mini.local>
2018-11-23 20:20:04 +0530

    X-Code Mac 86aa50669ed6fb45cabf33df02245098
    ca57e56dadc71eba961b04c993b71155769986b1da9a28e51eb2667a07cf9c5a

--- Code Template/Code_Template/Headers/static.h
+++ Code Template/Code_Template/Headers/static.h
@@ -117,6 +117,8 @@
 #include "constants.h"
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+#include <ext/pb_ds/trie_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
 
