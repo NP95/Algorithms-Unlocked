fde5c58dc0349a6176c02dead9f70f92520d81a6
System Administrator <root@SUMITs-Mac-mini.local>
2018-11-23 20:26:03 +0530

    X-Code Mac 2547eaecfc1dde69a67f9c93b9ce5f54
    34eb4b53182738c95ffa54587674aee9f0ec39eef362c282a0e21146b8366424

--- Code Template/Code_Template/Headers/static.h
+++ Code Template/Code_Template/Headers/static.h
@@ -131,5 +131,5 @@ auto rd_ints = std::bind(random_engine_block, seed);
 clock_t start_time, end_time;
 
 #endif /* static_h */
-typedef trie<string, mapped_type, cmp_fn, tag_type,
-trie_prefix_search_node_update> trie_type;
+typedef trie <std::string, mapped_type, cmp_fn, tag_type, trie_prefix_search_node_update> trie_type;
+typedef tree <int, null_mapped_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update> set_t;
