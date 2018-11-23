32ce9cd6a788414be741477b46739b08c1a6fb02
SpawnTree <sumit-google@google.com>
2018-11-23 20:38:51 +0530

    X-Code Mac f8a78b56ba1ac9c8b9e61fabb48ab61e
    8d68af62a8af8107b8ef941e431d36b295afb0103f997b56551097c8bd97e456

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -2,33 +2,6 @@
 
 typedef tree <int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics_node_update > ordered_set;
 
-// A PATRICIA trie with a prefix-search node-updator type. Note that
-// since the node updator is trie_prefix_search_node_update, then the
-// container includes its method prefix_range.
-typedef null_mapped_type        mapped_type;
-typedef string_trie_e_access_traits<>     cmp_fn;
-typedef pat_trie_tag             tag_type;
-
-typedef trie<string, mapped_type, cmp_fn, tag_type,
-trie_prefix_search_node_update> trie_type;
-
-// The following helper function takes a trie object and r_key, a
-// const reference to a string, and prints all entries whose key
-// includes r_key as a prefix.
-void
-print_prefix_match(const trie_type& t, const string& key)
-{
-    typedef trie_type::const_iterator         const_iterator;
-    typedef pair<const_iterator, const_iterator>     pair_type;
-    
-    cout << "All keys whose prefix matches \"" << key << "\":" << endl;
-    
-    const pair_type match_range = t.prefix_range(key);
-    for (const_iterator it = match_range.first; it != match_range.second; ++it)
-    cout << *it << ' ';
-    cout << endl;
-}
-
 void solution(int argc, char* argv[], char* envp[])
 {
     
