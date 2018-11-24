eb6d65346254e5bc8366379740b57ba4d76dc33d
SpawnTree <sumit-google@google.com>
2018-11-24 08:30:15 +0530

    X-Code Mac c64d7a36ea9cd41115c76957282a03e9
    b35dfb7cd161df1a113c83aec35dd740c9c3af687edbc7c80d6ecdd75a94e6f9

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -1,23 +1,8 @@
 #include "Headers/headers.h"
-const LL lr_mod  = 1000000003;
+
 void solution(int argc, char* argv[], char* envp[])
 {
-    static LL base = 2, expo = base % lr_mod;
-    static LL power = 1 << 30, counter = 0;
-    while(power)
-    {
-        expo %= lr_mod;
-        counter++; std::cout << power << ", " << counter << ", " << to_string(expo) << std::endl;
-        if (!(power&1)){
-            expo *= (expo % lr_mod);
-            power >>= 1;
-        } else {
-            expo *= (base % lr_mod);
-            --power;
-        }
-    }
-    if(expo < 0) std::cout << "Overflow." << std::endl;
-    else std::cout << to_string(expo) << std::endl;
+
 }
 
 int main(int argc, char* argv[], char* envp[])
