007c9c68a7d2c55223ed77c3c996896fc9a4e974
SpawnTree <sumit-google@google.com>
2018-11-24 17:02:01 +0530

    X-Code Mac aa7164945a2b832a7fab2dc29c7d0ce6
    7e4063de6169eb095e8cbbcfd53df13d3e2cbcbde6fb30c25a53286fee61a716

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -14,9 +14,9 @@ void solution(int argc, char* argv[], char* envp[])
     for(auto i = 0; i < 1000; i += h){
         for(auto j = 0; j < 1000; j += h){
             for (auto k = 0; k < 1000; k += h) {
-                for(auto il = 0; il < i + h; il++){
-                    for(auto jl = 0; j < j + h; jl++){
-                        for (auto kl = 0; k < k + h; kl++) {
+                for(auto il = i; il < i + h; il++){
+                    for(auto jl = j; jl < j + h; jl++){
+                        for (auto kl = k; kl < k + h; kl++) {
                             C[il][jl] += A[il][kl] * B[kl][jl];
                         }
                     }
