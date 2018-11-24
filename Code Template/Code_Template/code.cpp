14ede4375b21985b84212d2b048f8f0e9dba6f5f
SpawnTree <sumit-google@google.com>
2018-11-24 07:36:29 +0530

    X-Code Mac a894d49b753d701efde131dafebbadbf
    e08480ed9562562db5a4170654198ccb2b78a94af8175c513d5174bdc1a98de3

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -3,7 +3,7 @@
 void solution(int argc, char* argv[], char* envp[])
 {
     static LL base = 2, expo = base % MOD;
-    static int power = 32, counter = 0;
+    static int power = 33, counter = 0;
     while(power)
     {
         counter++;
