5e70c23f05d6749556d089ef1573d58624f1664b
SpawnTree <sumit-google@google.com>
2018-11-24 10:16:43 +0530

    X-Code Mac 112ee92927a4eecefdcc551e4bbdf249
    dcfff27db4e36b13b8ab996e568215550eccc00fa7a55c4ce92a92edc89a03f5

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -2,7 +2,7 @@
 
 void solution(int argc, char* argv[], char* envp[])
 {
-    huge_mod_power((1ULL << 60), (1ULL << 63));
+    huge_mod_power((1ULL << 30), (1ULL << 63));
 }
 
 int main(int argc, char* argv[], char* envp[])
