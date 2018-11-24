86e34182ae3bb5e763e3aa120975d76cea37d989
SpawnTree <sumit-google@google.com>
2018-11-24 07:28:36 +0530

    X-Code Mac c6f0b39707d742dbccbacc8455f7c95a
    7e140dfef94bc21802ac8bad01ea8885ec0cab12d71295433e8343ed00ba6ae2

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -208,7 +208,7 @@ typedef trie <string, null_type, trie_string_access_traits<>, pat_trie_tag, trie
 
 void solution(int argc, char* argv[], char* envp[])
 {
-    static LL base = 2, expo = base;
+    static LL base = 2, expo = base % MOD;
     static int power = 64, counter = 0;
     while(power)
     {
