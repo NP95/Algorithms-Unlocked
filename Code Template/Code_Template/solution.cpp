a0cb05436a28353b95fe934fff12d9b2cc1a4a3c
SpawnTree <sumit-google@google.com>
2018-11-25 09:24:52 +0530

    X-Code Mac 2b4f76b655b7ad008bf6260da141b6c7
    8b54f90acbfdf70ade7eec67b12d9b7ce5864d240e8e1d8b883a3780f82b0ae1

--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -219,6 +219,7 @@ void solution(int argc, char* argv[], char* envp[])
     std::cout << "unsigned int 64 : " <<  sizeof(uint64_t) << "B" <<  std::endl;
     std::cout << "long double : " <<  sizeof(long double) << "B" <<  std::endl;
     long double huge_integer = 1 << 127;
+    std::cout << "long double : " <<  huge_integer << std::endl;
 }
 
 int main(int argc, char* argv[], char* envp[])
