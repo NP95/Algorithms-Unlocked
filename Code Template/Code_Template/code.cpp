4b56852b66d701773f64fd636c9ae46d52d8b126
SpawnTree <sumit-google@google.com>
2018-11-25 11:42:30 +0530

    X-Code Mac 553156d77ab8d96a9f49ec023dd5c0ed
    7b997c096a6f5e90714db0ae9336c8d59c7b60f3a35d1b70565c635caad43411

--- Code Template/Code_Template/code.cpp
+++ Code Template/Code_Template/code.cpp
@@ -2,7 +2,7 @@
 
 void solution(int argc, char* argv[], char* envp[])
 {
-    assert(ripple_multiply(14541, 895414) == (14541 * 895414));
+    cout << to_string(ripple_multiply(15477454, 52447845584));
 }
 
 int main(int argc, char* argv[], char* envp[])
--- Code Template/Code_Template/solution.cpp
+++ Code Template/Code_Template/solution.cpp
@@ -204,10 +204,10 @@ typedef tree <int, null_type, std::less<int>, rb_tree_tag, tree_order_statistics
 typedef trie <string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> patricia_trie;
 
 /* Appended working code here using freopen() */
-
+811759117425463136.000000
 void solution(int argc, char* argv[], char* envp[])
 {
-    std::cout << 14541 * 895414 << std::endl;
+    cout << to_string(ripple_multiply(15477454, 52447845584));
 }
 
 int main(int argc, char* argv[], char* envp[])
